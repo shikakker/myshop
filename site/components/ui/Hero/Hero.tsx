1a42776cfafb3d3cbb13e752785636e4c2af5cef
Egor Denisov <designer.egor@ya.ru>
2026-09-15 18:19:17 -0400

    fix: make storefront hero CTA explicit

--- site/components/ui/Hero/Hero.tsx
+++ site/components/ui/Hero/Hero.tsx
@@ -1,15 +1,20 @@
 import React, { FC } from 'react'
+import Link from 'next/link'
 import { Container } from '@components/ui'
 import { ArrowRight } from '@components/icons'
 import s from './Hero.module.css'
-import Link from 'next/link'
+
 interface HeroProps {
   className?: string
   headline: string
   description: string
+  cta?: {
+    href: string
+    label: string
+  }
 }
 
-const Hero: FC<HeroProps> = ({ headline, description }) => {
+const Hero: FC<HeroProps> = ({ headline, description, cta }) => {
   return (
     <div className="bg-accent-9 border-b border-t border-accent-2">
       <Container>
@@ -17,12 +22,14 @@ const Hero: FC<HeroProps> = ({ headline, description }) => {
           <h2 className={s.title}>{headline}</h2>
           <div className={s.description}>
             <p>{description}</p>
-            <Link href="/">
-              <a className="flex items-center text-accent-0 pt-3 font-bold hover:underline cursor-pointer w-max-content">
-                Read it here
-                <ArrowRight width="20" heigh="20" className="ml-1" />
-              </a>
-            </Link>
+            {cta && (
+              <Link href={cta.href}>
+                <a className="flex items-center text-accent-0 pt-3 font-bold hover:underline cursor-pointer w-max-content">
+                  {cta.label}
+                  <ArrowRight width="20" heigh="20" className="ml-1" />
+                </a>
+              </Link>
+            )}
           </div>
         </div>
       </Container>
